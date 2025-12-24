5267e207a838eada23e098259ba929867ee89e8e
Web and app course <fizaabrar720@gmail.com>
2025-12-24 12:12:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// variable types 
+// let,const,var
+// these are container(buney to store information->name,amount,age)
+// var fathername = "jhon@gmail.com"
+var username="jhon"
+// var password="password123"
+
+// var gmail = "nimrah@gmail.com"
+// var username="jhon"
+// var password="password123"
+
+// var gmail = "saba497@gmail.com"
+// var username="saba"
+// var password="password123"
+
+// make variables for the the fields of intermediate exam form
+
+
+function change(){
+    document.getElementById("username").style.color="red"
+    document.getElementById("username").innerHTML=username
+
+}
