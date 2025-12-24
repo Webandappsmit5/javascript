d04e5893320537c54cd4dd7866b87471072172ba
Web and app course <fizaabrar720@gmail.com>
2025-12-24 13:06:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // let,const,var
 // these are container(buney to store information->name,amount,age)
 // var fathername = "jhon@gmail.com"
-var username="jhon"
+// const username="jhon"
 // var password="password123"
 
 // var gmail = "nimrah@gmail.com"
@@ -11,15 +11,26 @@ var username="jhon"
 
 // var gmail = "saba497@gmail.com"
 // var username="saba"
+
 // var password="password123"
 
 // make variables for the the fields of intermediate exam form
 // document.getElementById("username").style.color="red"
 // document.getElementById("username").innerHTML=username
 
+// diffrence between var let const 
+// 1.const value can not be changed. var/let value can be changed
+const username1="kulsoom"
+username1="jhon"
 
+//2.you can not redeclare const and let variable  var variable
+var age=30
+
+var age=40
+
+//3.global scope and block scope
 function change(){
-    var favcolor=prompt("enter you favourit color")
-    document.body.style.backgroundColor=favcolor
+    var username=prompt("enter your name")
+    document.getElementById("username").innerHTML=username
 
 }
