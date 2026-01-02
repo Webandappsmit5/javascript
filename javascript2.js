b414804f402162f88913be5f0af6e0e3735ca07c
Web and app course <fizaabrar720@gmail.com>
2026-01-02 12:24:14 +0500

    Create javascript2.js

---
+++ javascript2.js
@@ -0,0 +1,19 @@
+var marks=80
+if(marks>=50 && marks<=100){
+    if(marks==100){
+        console.log("you got full marks")
+    }
+    else if(marks==50){
+        console.log("improve your marks are barely passing ")
+    }
+    else{
+        console.log("good")
+    }
+   
+}
+else if(marks<50){
+    console.log("fail")
+}
+else{
+    console.log("invalid marks")
+}
