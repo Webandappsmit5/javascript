7c137c25c08bc3d315b6c1929c492e2d30e17e97
Web and app course <fizaabrar720@gmail.com>
2026-01-07 12:31:08 +0500

    Create javscript2.js

---
+++ javscript2.js
@@ -0,0 +1,12 @@
+var isFriends=false
+while(isFriends==false){
+    var friend1=prompt("enter your fav dish1")
+    var friend2=prompt("enter your fav dish2")
+    if(friend1==friend2){
+
+    console.log("best friends")
+    isFriends=true
+}else{
+    console.log("not right")
+    isFriends=false
+}
